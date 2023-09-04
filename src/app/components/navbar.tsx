709db5cb38ded84b7630698865d735aae08b9ed7
Victor Martins <martinsvictor287@gmail.com>
2023-09-05 00:43:21 +0300

    update Navbar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -95,7 +95,7 @@ export default function Navbar() {
 				<Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
 					<div className="flex items-center justify-between">
 						<a href="#" className="-m-1.5 p-1.5">
-							<span className="sr-only">Your Company</span>
+							<span className="sr-only">ReacType</span>
 							<Image
 								className="h-8 w-auto"
 								src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
