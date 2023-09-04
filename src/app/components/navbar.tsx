d3e89ff14d015918fab7d52ae69cd4fa14670dd2
Victor Martins <martinsvictor287@gmail.com>
2023-09-05 00:54:11 +0300

    update menu text

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -43,7 +43,7 @@ export default function Navbar() {
 						className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
 						onClick={() => setMobileMenuOpen(true)}
 					>
-						<span className="sr-only">Open main menu</span>
+						<span className="sr-only">Open menu</span>
 						<Bars3Icon className="h-6 w-6" aria-hidden="true" />
 					</button>
 				</div>
