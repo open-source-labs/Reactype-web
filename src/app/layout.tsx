df2bece838e58057d9a34099e4ad7f5cbd40c6a6
Victor Martins <martinsvictor287@gmail.com>
2023-09-05 00:53:32 +0300

    fix layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css';
 
 import { Inter } from 'next/font/google';
 import type { Metadata } from 'next';
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
