433fef057c8557e96bd04f18c8bb6165c82c88f7
Victor Martins <martinsvictor287@gmail.com>
2023-09-05 00:42:35 +0300

    fix navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css';
 
 import { Inter } from 'next/font/google';
 import type { Metadata } from 'next';
-import Navbar from './components/Navbar';
+import Navbar from './components/navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
