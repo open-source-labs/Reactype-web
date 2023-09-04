a8110a506bbb5c553b9409e5a1242c4414588d0c
Victor Martins <martinsvictor287@gmail.com>
2023-09-05 00:47:33 +0300

    fix navbar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
-import { Disclosure, Menu, Transition } from '@headlessui/react';
 import { Fragment, useState } from 'react';
 
 import { Dialog } from '@headlessui/react';
