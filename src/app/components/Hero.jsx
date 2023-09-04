44f8eed4949bc5db0966a91797a0230fac2bf40e
Victor Martins <martinsvictor287@gmail.com>
2023-09-05 00:40:14 +0300

    add hero

---
+++ src/app/components/Hero.jsx
@@ -0,0 +1,46 @@
+import Image from 'next/image';
+import React from 'react';
+import heroImage from 'public/herodemo.png';
+
+const Hero = () => {
+	return (
+		<div>
+			{' '}
+			<div className="relative isolate px-6 lg:px-8">
+				<div
+					className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+					aria-hidden="true"
+				>
+					<div className="relative " />
+				</div>
+				<div className="mx-auto max-w-3xl py-20 sm:py-48 lg:py-40">
+					<div className="text-center">
+						<h1 className="text-4xl font-bold tracking-tight sm:text-6xl">
+							A visual prototyping tool for React developers
+						</h1>
+						<p className="mt-6  text-lg leading-8 ">
+							A comprehensive type checking with TypeScript and flexible
+							exporting in Classic React, Next.js, or Gatsby.js
+						</p>
+						<div className="mt-10 flex items-center justify-center gap-x-6">
+							<a
+								href="#"
+								className="rounded-md bg-white px-6 py-3 text-lg  font-medium text-black shadow-sm"
+							>
+								Try Here
+							</a>
+							<a href="#" className="text-lg font-medium leading-6">
+								Download <span aria-hidden="true">→</span>
+							</a>
+						</div>
+					</div>
+				</div>
+				<div className="flex justify-center">
+					<Image width={1100} height={589} src={heroImage} alt="logo" />
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Hero;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import Hero from './components/Hero';
 import Image from 'next/image';
-
 export default function Home() {
-	return <div>Hello World</div>;
+	return (
+		<div>
+			<Hero />
+		</div>
+	);
 }
