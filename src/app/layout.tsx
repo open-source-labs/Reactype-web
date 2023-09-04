ba4d8f7639c0cc30164ddd7948e3f92e0db8717d
Victor Martins <martinsvictor287@gmail.com>
2023-09-04 23:50:32 +0300

    add navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 
 import { Inter } from 'next/font/google';
 import type { Metadata } from 'next';
+import Navbar from './components/Navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,7 +18,10 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>{children}</body>
+			<body className={inter.className}>
+				<Navbar />
+				{children}
+			</body>
 		</html>
 	);
 }
