fbd33d44580a6a2478b9748f0fca79c9855d1cdf
Victor Martins <martinsvictor287@gmail.com>
2023-09-14 23:39:23 +0300

    UI: change CTA text

---
+++ src/app/container/TutorialContainer.tsx
@@ -0,0 +1,14 @@
+import Hero from '../components/Hero';
+import React from 'react';
+import Team from '../components/Team';
+
+const TutorialContainer = () => {
+	return (
+		<div>
+			<Hero />
+			<Team />
+		</div>
+	);
+};
+
+export default TutorialContainer;
---
+++ src/app/tutorials/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TutorialContainer from '../container/TutorialContainer';
+
+const page = () => {
+	return (
+		<div>
+			{' '}
+			<TutorialContainer />{' '}
+		</div>
+	);
+};
+
+export default page;
